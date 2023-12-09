a82d53dfb1b3d91dca8bfc730afe9159bc158629
jarrettstowe <okanella@gmail.com>
2023-12-08 21:36:00 -0600

    Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,24 @@
+// Copyright Jarry
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = WorldContextObject->GetWorld()->GetFirstPlayerController())
+	{
+		if (AAuraHUD* AuraHud = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return AuraHud->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
